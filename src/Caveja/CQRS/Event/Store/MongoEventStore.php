3aeb464cf5b36ba530c11e938d049b4d5f434220
Davide Bellettini <davide@bellettini.me>
2014-05-12 19:38:45 +0200

    Better PHPDoc

--- src/Caveja/CQRS/Event/Store/MongoEventStore.php
+++ src/Caveja/CQRS/Event/Store/MongoEventStore.php
@@ -172,7 +172,7 @@ class MongoEventStore implements EventStoreInterface
 
     /**
      * @param  UUID                 $aggregateId
-     * @param $expectedVersion
+     * @param int $expectedVersion
      * @return 0
      * @throws ConcurrencyException
      */
